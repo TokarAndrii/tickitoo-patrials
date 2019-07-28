f88161c6d57f13051281e2155289e9a0b1e50c74
TokarAndrii <tokar.andrii@gmail.com>
2019-07-28 21:49:22 +0300

    seat and part of route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Seat from "./seat/Seat";
+import RouteItem from "./route/RouteItem";
+import styles from "./App.module.sass";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div
+        style={{
+          marginBottom: "32px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Seat seatNumber={32} isFree handleChoseSeat={() => null} />
+        <Seat seatNumber={33} isFree={false} handleChoseSeat={() => null} />
+      </div>
+
+      <div>
+        <RouteItem
+          routeSheduleTime="16:33"
+          routeSheduleDescriptionTitle="Киев"
+          routeSheduleDescriptionText={`Автовокзал "Центральный", метро Демеевская; проспект Науки; дом 1/2`}
+        />
+        <RouteItem
+          routeSheduleTime="16:43"
+          routeSheduleDescriptionTitle="Одесса"
+          routeSheduleDescriptionText={`Автовокзал "Центральный", метро Демеевская; проспект Науки; дом 1/2`}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/route/RouteItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./RouteItem.sass";
+
+const RouteItem = ({
+  isActive,
+  routeSheduleTime,
+  routeSheduleDescriptionTitle,
+  routeSheduleDescriptionText
+}) => {
+  return (
+    <div className="route__schedule-inner">
+      <div className="route__schedule-time">{routeSheduleTime}</div>
+      <div className="route__schedule-check">
+        <span />
+      </div>
+      <div className="route__schedule-description">
+        <strong>{routeSheduleDescriptionTitle}</strong>
+        <p>{routeSheduleDescriptionText}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RouteItem;
