adcbdb38367d02c1d3db2db434ce1d4b4a26b323
TokarAndrii <tokar.andrii@gmail.com>
2019-08-02 21:21:22 +0300

    test big bus mock data

--- src/App.js
+++ src/App.js
@@ -1,69 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./App.module.sass";
 import testRouteData from "./route/testDataRoutes";
 import RouteList from "./route/RouteList";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Bus from "./bus/Bus";
-import mockDataSeats2 from "./bus/mockDataSeatsCart2";
-import utils from './utils/index';
-
+import smallBusMockData from "./bus/mockDataSeatsCart2";
+import bigBusMockData from "./bus/bigBusMockData";
+import utils from "./utils/index";
 
 //some stuff to get array from income request
-const {maps_seat} = mockDataSeats2;
-const [first_element ] = maps_seat;
-const {map_seat} = first_element;
+//const { maps_seat } = mockDataSeats2;
 
+const { maps_seat } = bigBusMockData;
+const [first_element] = maps_seat;
+const { map_seat } = first_element;
 const sortedArray = utils.sortArray(map_seat);
-const chunkedArray = utils.arrayTransform(sortedArray)
+const chunkedArray = utils.arrayTransform(sortedArray);
 
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className={styles.app}>
+          <div
+            style={{
+              marginBottom: "32px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          />
 
+          <div>
+            <RouteList listRoute={testRouteData} fullView={true} />
+          </div>
 
-function App() {
-  return  (
-    <>
-      <Header />
-      <div className={styles.app}>
+          <br />
+          <br />
+          <div>
+            <RouteList listRoute={testRouteData} fullView={false} />
+          </div>
+        </div>
+        <div style={{ marginTop: "300px" }} />
+
+        {/* some bus holder */}
         <div
           style={{
-            marginBottom: "32px",
+            margin: "24px 0",
             display: "flex",
             justifyContent: "center",
             alignItems: "center"
           }}
         >
+          <Bus
+            title="Выберите место на схеме автобуса"
+            busName="Автобус Hyndai Universe Bus"
+            seatsList={chunkedArray}
+          />
         </div>
-
-        <div>
-          <RouteList listRoute={testRouteData} fullView={true} />
-        </div>
-
-        <br />
-        <br />
-        <div>
-          <RouteList listRoute={testRouteData} fullView={false} />
-        </div>
-      </div>
-      <div style={{ marginTop: "300px" }} />
-
-      {/* some bus holder */}
-      <div
-        style={{
-          margin: "24px 0",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Bus
-          title="Выберите место на схеме автобуса"
-          busName="Автобус Hyndai Universe Bus"
-          seatsList={chunkedArray}
-        />
-      </div>
-      <Footer copirightText="&copy; 2018-2019 Tickitoo. Все права защищены." />
-    </>
-  );
+        <Footer copirightText="&copy; 2018-2019 Tickitoo. Все права защищены." />
+      </>
+    );
+  }
 }
 
 export default App;
