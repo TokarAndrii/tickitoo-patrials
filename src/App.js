90983c1c42d6a06be1d2e621903a356e8099008d
TokarAndrii <tokar.andrii@gmail.com>
2019-08-12 21:27:40 +0300

    paymentPage part2

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import utils from "./utils/index";
 import Seat from "./seat/Seat";
 import BusFreeSeating from "./busFreeSeating/BusFreeSeating";
 import mockBusFreeSeatingObjectInfo from "./busFreeSeating/mockObjectInfo";
-import PaymentPage from './paymentPage/PaymentPage';
-import paymentPageMockData from './paymentPage//paymentPageListMockData';
+import PaymentPage from "./paymentPage/PaymentPage";
+import paymentPageMockData from "./paymentPage//paymentPageListMockData";
 
 //some stuff to get array from income request
 //const { maps_seat } = mockDataSeats2;
@@ -26,7 +26,6 @@ const { map_seat } = first_element;
 const sortedArray = utils.sortArray(map_seat);
 const chunkedArray = utils.arrayTransform(sortedArray);
 
-
 const mockPassengerData = {
   fullName: "Иванов Иван",
   seatNumber: 39,
@@ -34,82 +33,75 @@ const mockPassengerData = {
   ageCategory: "взрослый"
 };
 
-
 class App extends Component {
   render() {
-
     return (
-      (
-        <>
-          <Header />
-          <div className={styles.app}>
-            <div
-              style={{
-                marginBottom: "32px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            />
-
-            <div>
-              <RouteList listRoute={testRouteData} fullView={true} />
-            </div>
-
-            <br />
-            <br />
-            <div>
-              <RouteList listRoute={testRouteData} fullView={false} />
-            </div>
-          </div>
-          <div style={{ marginTop: "300px" }} />
-
-          <ButtonBack
-            className={styles.buttonBack}
-            message="назад"
-            linkTo="/"
-          />
-
-          <main>
-            <Passenger objectInfo={mockPassengerData} seatMessage="место" />
-            <TnankYou
-              title="Билеты успешно оплачены!"
-              textAboutDownLoad="Вы можете скачать их, нажав на кнопку"
-              downloadBtnMessage="Скачать билеты"
-              textAboutSendToMail="Мы также выслали копию билетов на почту"
-              email="example@mail.com"
-              textAboutPrintTickets="Не забудьте распечатать их перед посадкой в автобус"
-              textAboutThank="Благодарим за покупку! Приятного пути :)"
-              backWaysBtnMessage="Искать обратный билет"
-            />
-          </main>
-
-          {/* some bus holder */}
+      <>
+        <Header />
+        <div className={styles.app}>
           <div
             style={{
-              margin: "24px 0",
+              marginBottom: "32px",
               display: "flex",
               justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column"
+              alignItems: "center"
             }}
-          >
-            <Bus
-              title="Выберите место на схеме автобуса"
-              busName="Hyndai Universe Bus"
-              seatsList={chunkedArray}
-            />
-            <br />
-            <br />
-            <BusFreeSeating objectInfo={mockBusFreeSeatingObjectInfo} />
+          />
+
+          <div>
+            <RouteList listRoute={testRouteData} fullView={true} />
           </div>
 
-          <PaymentPage paymentPageInfo={paymentPageMockData}/>
+          <br />
+          <br />
+          <div>
+            <RouteList listRoute={testRouteData} fullView={false} />
+          </div>
+        </div>
+        <div style={{ marginTop: "300px" }} />
 
+        <ButtonBack className={styles.buttonBack} message="назад" linkTo="/" />
 
-          <Footer copirightText="&copy; 2018-2019 Tickitoo. Все права защищены." />
-        </>
-      )
+        <main>
+          <Passenger objectInfo={mockPassengerData} seatMessage="место" />
+          <TnankYou
+            title="Билеты успешно оплачены!"
+            textAboutDownLoad="Вы можете скачать их, нажав на кнопку"
+            downloadBtnMessage="Скачать билеты"
+            textAboutSendToMail="Мы также выслали копию билетов на почту"
+            email="example@mail.com"
+            textAboutPrintTickets="Не забудьте распечатать их перед посадкой в автобус"
+            textAboutThank="Благодарим за покупку! Приятного пути :)"
+            backWaysBtnMessage="Искать обратный билет"
+          />
+        </main>
+
+        {/* some bus holder */}
+        <div
+          style={{
+            margin: "24px 0",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column"
+          }}
+        >
+          <Bus
+            title="Выберите место на схеме автобуса"
+            busName="Hyndai Universe Bus"
+            seatsList={chunkedArray}
+          />
+          <br />
+          <br />
+          <BusFreeSeating objectInfo={mockBusFreeSeatingObjectInfo} />
+        </div>
+
+        <div className={styles.container}>
+          <PaymentPage paymentPageInfo={paymentPageMockData} />
+        </div>
+
+        <Footer copirightText="&copy; 2018-2019 Tickitoo. Все права защищены." />
+      </>
     );
   }
 }
